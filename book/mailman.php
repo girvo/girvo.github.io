0460accb09d98f2fbe4e500c803f97a314e8f098
Josh Girvin <josh.girvin@gmail.com>
2014-03-15 18:07:37 +1000

    Moving my blog into Github

---
+++ book/mailman.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * A simple mailing-list setup, using SQLite3 and PHP5.3
+ * 
+ * @author Josh Girvin <josh@jgirvin.com>
+ * @copyright Copyright (c) 2013, Josh Girvin
+ * @license Proprietary
+ */
